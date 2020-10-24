e345f127bda892890ca89c4438d3a382fd377c8b
helenayyan <helenayyan@outlook.com>
2020-10-24 16:20:39 +0100

    dialogue_manager added

--- infra/dialogue_manager.py
+++ infra/dialogue_manager.py
@@ -17,25 +17,28 @@ class DialogueManager(Component):
         ["state=greeting, abort, thanks, problem, yes, no"]
         """
         self.input = imput
-        self.setup()
+        self.setup1()
         self.last_state = None
         # self.learnparameter=1
         self.state_counter = 0
-        self.filename = 'doc/cnc_troubleshooting.xlsx'
+        self.filename = '../doc/cnc_troubleshooting.xlsx'
         self.df = self.excel_to_df()
         self.df_stats = self.get_solutions(self.part, self.trouble)
 
     def setup(self):
-        #print("Dialogue Manager is setup!")
+        pass
+
+    def setup1(self):
+        # print("Dialogue Manager is setup!")
         self.part = self.input["parts"]
         # self.part_tracker=[]
         self.trouble = self.input["trouble"]
         self.state = self.input["states"]
         pass
 
-    def input_debug(self,intm):
-        self.input=intm
-        self.setup()
+    def input_debug(self, intm):
+        self.input = intm
+        self.setup1()
 
     def do_step(self):
         # self.tracker.input = self.input
@@ -44,8 +47,9 @@ class DialogueManager(Component):
         if self.state == "greeting":
             response_msg = self.greeting()
         elif self.state == "thanks":
-            response_msg = self.thanks()
             print(1111)
+            response_msg = self.thanks()
+
         elif self.state == "trouble" or "yes" or "no":
             response_msg = self.trouble_shooting(self.part, self.trouble)
         self.output = response_msg
@@ -60,13 +64,16 @@ class DialogueManager(Component):
 
     def thanks(self):
         print(self.part, self.trouble, self.state_counter)
-        filtered_df = self.df[
-            self.df["Parts"] == self.part and self.df["Error"] == self.trouble and self.df["Solution"] == self.df_stats[
-                self.state_counter - 1]]
-        filtered_df["appear_time"] = filtered_df["appear_time"] + 1
+        i = 0
+        while self.df.loc[i, 'Parts'] != self.part or self.df.loc[i, 'Error'] != self.trouble or self.df.loc[
+            i, 'Solution'] != self.df_stats[self.state_counter - 1]:
+            i += 1
+        self.df.loc[i, 'appear_time'] += 1
+        self.df.to_excel(self.filename, sheet_name='Sheet1', index=False, header=True)
 
-        self.df.to_excel(self.filepath)
+        self.df.to_excel(self.filename)
         # self.init()
+
         return "My pleasure"
 
     def trouble_shooting(self, part, trouble):
@@ -108,7 +115,7 @@ class DialogueManager(Component):
 
 if __name__ == '__main__':
     input = {"parts": "Tool magazine(Umbrella type)", "trouble": "Noise for tool changing", "states": "trouble"}
-    m=DialogueManager(imput=input)
+    m = DialogueManager(imput=input)
 
     m.run()
     input = {"parts": "Tool magazine(Umbrella type)", "trouble": "Noise for tool changing", "states": "trouble"}
