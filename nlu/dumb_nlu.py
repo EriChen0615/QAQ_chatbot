19b14e0dda8b7c227bb918b4055a96f4c7c215cb
coconutgit <43692301+coconutgit@users.noreply.github.com>
2020-10-24 16:35:26 +0100

    update dumb_nlu.py

--- nlu/dumb_nlu.py
+++ nlu/dumb_nlu.py
@@ -2,6 +2,9 @@ import sys
 sys.path.append('..')
 from infra.nlu import NLU
 import re
+import spacy
+import numpy as np
+from nltk.stem.snowball import SnowballStemmer
 
 class Dumb_NLU(NLU):
 
@@ -11,4 +14,25 @@ class Dumb_NLU(NLU):
     def process(self):
         num = re.search(r'\d+', self.text).group(0)
         intent = re.search(r'hello', self.text).group(0)
+
+        text = self.text
+        text = text.replace('.',' ')
+        text = text.replace(',',' ')
+
+        nlp = spacy.load('en_core_web_sm')
+        lemmatizer = nlp.vocab.morphology.lemmatizer
+        stemmer = SnowballStemmer(language='english')
+        stemmed_text = stemmer.stem(text.lower())
+
+        stemmed_text_list = stemmed_text.split(' ')
+
+        # load word_error_mat file to a matrix
+        probability_matrix = np.loadtxt(open("test.csv", "rb"), delimiter=",", skiprows=1, skipcols=1)
+        print(probability_matrix)
+
+        # num_stemmed_text_list = []
+        # for word in stemmed_text_list:
+
+
+
         return {'number':num, 'intent':intent}
